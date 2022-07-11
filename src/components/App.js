3f131538819123c6fb778741ad371531ef0de1a6
Kelsey Weeks <kelsey@kelseyweeks.com>
2022-07-11 17:01:54 -0500

    kwcommit

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React from "react"
+import Header from "./Header"
+import NavBar from "./Navbar"
+import MovieContainer from "./MovieContainer"
+// import NewForm from "./NewForm"
+import { useState, useEffect } from 'react'
 
 function App() {
-  return (
-    <h1>hi</h1>
-  );
-}
+    const [movies, setMovies] = useState([])
+    const [search, setSearch] = useState("")
 
-export default App;
+    function fetchMovies() {
+        fetch("http://localhost:3000/movies")
+          .then((res) => res.json())
+          .then((moviesData) => setMovies(moviesData))
+    }
+    useEffect(fetchMovies, [])
+
+    const filteredMovies = movies.filter(
+        (movie) => movie.name.toLowerCase().includes(search.toLowerCase()) || movies.description.toLowerCase().includes(search.toLowerCase())
+    )
+    return (
+        <div>
+            <Header />
+            {/* <NavBar setSearch={setSearch} search={search}/> */}
+            <MovieContainer movies={filteredMovies}/>
+            {/* <NewForm /> */}
+        </div>
+    )
+}
+export default App
